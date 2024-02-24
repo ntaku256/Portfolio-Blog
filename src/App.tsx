68cf25011b71a8d070ec31001b0ef28a391f8143
ntaku256 <nakaya2004t@icloud.com>
2024-02-25 02:27:37 +0900

    7st commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
-import Blog from './components/content/Blog';
 
 function App() {
   return (
     <div className="App">
-      <Blog />
-      {/* <header className="App-header">
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -20,7 +18,7 @@ function App() {
         >
           Learn React
         </a>
-      </header> */}
+      </header>
     </div>
   );
 }
