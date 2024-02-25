fe4b05e0d0a60095e14dba4ee9a80abf8c46d71b
ntaku256 <nakaya2004t@icloud.com>
2024-02-26 01:10:09 +0900

    12st commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,11 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
-import Blog from "./components/content/Blog";
-import Article from './components/content/Article';
 import RouterComponent from './components/content/Router';
 
 function App() {
   return (
     <div className="App">
       <RouterComponent/>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
     </div>
   );
 }
