1a1f4b019f7ca185bc1028d243c704a9eed43092
ntaku256 <nakaya2004t@icloud.com>
2024-02-26 01:17:15 +0900

    12st commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import RouterComponent from './components/content/Router';
+import Blog from './Portfolio-Blog/components/content/Blog';
 
 function App() {
   return (
     <div className="App">
-      <RouterComponent/>
+      <Blog/>
     </div>
   );
 }
