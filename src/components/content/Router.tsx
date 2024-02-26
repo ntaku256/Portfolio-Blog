e70632b52d1c9668e26c69b78485ae73ec8ce023
ntaku256 <nakaya2004t@icloud.com>
2024-02-26 11:39:11 +0900

    12st commit

--- src/components/content/Router.tsx
+++ src/components/content/Router.tsx
@@ -1,21 +1,27 @@
 import React from 'react';
-import { BrowserRouter , Routes, Route } from 'react-router-dom';
+import { BrowserRouter , Routes, Route , useLocation } from 'react-router-dom';
 import Blog from './Blog';
 import Article from './Article';
+
+const PathDisplay = () => {
+  const location = useLocation();
+
+  return <div>Current Path: {location.pathname}</div>;
+};
 
 const RouterComponent: React.FC = () => {
   return (
     <div>
       <h1>oooooooooooooooooooooooooo</h1>
+      <PathDisplay />
       <Blog/>
       <Article/>
-      {/* <BrowserRouter>
+      <BrowserRouter>
       <Routes>
-      <Route path='/' element={<Blog/>}/>
-        <Route path='/Portfolio-Blog/' element={<Blog/>}/>
-        <Route path='/Portfolio-Blog/Article/:postId' element={<Article />}/>
+        <Route path='/' element={<Blog/>}/>
+        <Route path='/Article/:postId' element={<Article />}/>
       </Routes>
-      </BrowserRouter> */}
+      </BrowserRouter>
     </div>
   );
 };
